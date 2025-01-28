e3eea3192f27c11bb5846b83fa9b80e546b571d3
imfeelingcrazy <lightningmcqueenwow@gmail.com>
2025-01-27 20:43:55 -0600

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -1,12 +1,16 @@
 incrementValue = 1;
 
+function gamble() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
 function increment() {
     countElement = document.getElementById("count");
     count = parseInt(countElement.textContent);
     count += incrementValue;
     countElement.textContent = count
 
-    if (count === 10) {
+    if (count === 1) {
         showDoubleButton();
     }
 }
@@ -17,7 +21,14 @@ function showDoubleButton() {
 }
 
 function doubleClick() {
-    incrementValue *= 2;
-    alert("The value of extortion has now doubled! It is now worth " + incrementValue + "money per click!")
-    doubleButton.style.display = "none";
+    if (gamble() >= 8) {
+        incrementValue *= 2;
+        alert("The value of extortion has now doubled! It is now worth " + incrementValue + " money per click!")
+        doubleButton.style.display = "none";
+    }
+    else {
+        count = 0;
+        countElement.textContent = count
+        doubleButton.style.display = "none";
+    }
 }
