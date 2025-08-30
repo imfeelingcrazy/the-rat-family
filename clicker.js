284a4810085f77b04a8fbfef7e89b8ca1ddb30b4
imfeelingcrazy <lightningmcqueenwow@gmail.com>
2025-08-29 20:56:42 -0500

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -1,6 +1,15 @@
+cheese = 0;
+
 const introScreen = document.getElementById('intro-screen');
 function hideIntro() {
     introScreen.style.display = 'none';
 }
 document.addEventListener('keydown', hideIntro);
 document.addEventListener('click', hideIntro);
+
+const extort_button = document.getElementById('extort');
+const cheeseCount = document.getElementById('cheese-count');
+extort_button.addEventListener('click', function() {
+    cheese += 1;
+    cheeseCount.textContent = cheese;
+});
